473b7e3c8c633bfc46ab9c5725051b03a07e3bf2
yuki178504 <aerosmit1989x@gmail.com>
2022-10-05 16:32:52 +0900

    [add]api通信のテストを実装 routesにformat jsonを追加
    api通信ができるかどうかを仮のファイルで実装しました。

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :test, only: %i[index]
-      resources :scene_posts
+      resources :test, only: %i[index], format: 'json'
+      resources :scene_posts, format: 'json'
     end
   end
 end
--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import instance from './api/api';
 
-function App() {
+const App = () => {
+
+  const [ posts, setPost ] = useState([])
+
+  useEffect(() => {
+    handleGetList();
+  }, []);
+
+  const handleGetList = async () => {
+    try {
+      const res = await instance.get('/api/v1/scene_posts');
+      console.log(res.data);
+      setPost(res.data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+
+
   return (
     <div className="App">
-      テスト
+      <p>
+        { posts.map((post) => (
+          <div>
+            <div>APIのテスト</div>
+            <div>{ post.scene_title }</div>
+          </div>
+        )) }
+      </p>
     </div>
   );
 }
