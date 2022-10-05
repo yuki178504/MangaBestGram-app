09ca48aec3a38e0c05c92e0ce00690bc5d7c486c
yuki178504 <aerosmit1989x@gmail.com>
2022-10-05 18:14:39 +0900

    [add]Header コンポーネント実装 css.module実装

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,38 +1,10 @@
-import './App.css';
-import React, { useEffect, useState } from 'react';
-import instance from './api/api';
+import React from 'react'
+import './css/App.css';
+import Header from './components/Header';
 
 const App = () => {
-
-  const [ posts, setPost ] = useState([])
-
-  useEffect(() => {
-    handleGetList();
-  }, []);
-
-  const handleGetList = async () => {
-    try {
-      const res = await instance.get('/api/v1/scene_posts');
-      console.log(res.data);
-      setPost(res.data)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-
-
   return (
-    <div className="App">
-      <p>
-        { posts.map((post) => (
-          <div>
-            <div>APIのテスト</div>
-            <div>{ post.scene_title }</div>
-          </div>
-        )) }
-      </p>
-    </div>
+    <Header />
   );
 }
 
---
+++ frontend/app/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import header from '../css/header.module.css';
+
+const Header = () => {
+  return <nav className={header["navbar"]}>
+  <h4 className={header["navbar-logo"]}>
+    <p>MangaBestGram</p>
+  </h4>
+  <div className={header["nav-list"]}>
+    <ul className={header["nav-ul"]}>
+      <li className={header["nav-li"]}>
+        <p to="/posts" className={header["nav-li-text"]}>新規投稿</p>
+      </li>
+      <li className={header["nav-li"]}>
+        <p to="/" className={header["nav-li-text"]}>ホーム</p>
+      </li>
+      <li className={header["nav-li"]}>
+        <p to="/lists" className={header["nav-li-text"]}>投稿一覧</p>
+      </li>
+    </ul>
+  </div>
+</nav>
+};
+
+export default Header;
