1c8a93fe5ba9293862f0731eea2dc232245636ca
yuki178504 <aerosmit1989x@gmail.com>
2022-10-07 04:19:12 +0900

    [update]ヘッダー　フッターにリンク追加

--- frontend/app/src/components/Header.js
+++ frontend/app/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import header from '../css/header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return <nav className={header["navbar"]}>
   <h4 className={header["navbar-logo"]}>
-    <p>MangaBestGram</p>
+    <p><Link to='/' className={header["navbar-logo"]}>MangaBestGram</Link></p>
   </h4>
   <div className={header["nav-list"]}>
     <ul className={header["nav-ul"]}>
--- frontend/app/src/components/Home.js
+++ frontend/app/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import home from '../css/home.module.css'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -9,7 +10,7 @@ const Home = () => {
       <div className={home["top-title-text"]}>あなたのお気に入りの場面を共有しよう！</div>
       <div className={home["top-post"]}>
         <div className={home["top-post-title"]}>
-          <p to="" className={home["top-post-title-link"]}>最新の投稿</p>
+          <Link to="" className={home["top-post-title-link"]}>最新の投稿</Link>
         </div>
       </div>
     </div>
