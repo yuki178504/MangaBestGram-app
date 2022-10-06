8ea1dcd417c81d1d2823bfb9c35cc6fc12edc1e5
yuki178504 <aerosmit1989x@gmail.com>
2022-10-07 03:51:36 +0900

    [add]Home.js実装 home.module.css実装

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './css/App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 
 const App = () => {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Home />
+    </div>
   );
 }
 
