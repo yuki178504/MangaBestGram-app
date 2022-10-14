0626b2005de0e735ecfda1b557f2d36590df1320
yuki178504 <aerosmit1989x@gmail.com>
2022-10-15 01:58:51 +0900

    [update]マイページタブ化実装

--- frontend/app/src/components/MyPage.js
+++ frontend/app/src/components/MyPage.js
@@ -3,6 +3,7 @@ import Profile from "./model/mypage/Profile";
 import mypage from "../css/mypage.module.css"
 import { AiFillHome, AiFillEdit, AiFillHeart, AiFillFileText, AiOutlineUser } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const MyPage = () => {
   return (
@@ -11,26 +12,50 @@ const MyPage = () => {
         <div className={mypage.title}><span className={mypage.home}><Link to='/' className={mypage["home-link"]}><span className={mypage["react-icons"]}><AiFillHome /></span>ホーム</Link></span> / マイページ</div>
       </div>
       <div className={mypage.menu}>
-        <ul className={mypage["menu-list"]}>
-          <ul className={mypage["menu-list-in"]}>
-            <li>新規投稿</li>
-            <li className={mypage["menu-icon"]}><AiFillEdit /></li>
-          </ul>
-          <ul className={mypage["menu-list-in"]}>
-            <li>お気に入り</li>
-            <li className={mypage["menu-icon"]}><AiFillHeart /></li>
-          </ul>
-          <ul className={mypage["menu-list-in"]}>
-            <li>投稿一覧</li>
-            <li className={mypage["menu-icon"]}><AiFillFileText /></li>
-          </ul>
-          <ul className={mypage["menu-list-in-end"]}>
-            <li>プロフィール</li>
-            <li className={mypage["menu-icon"]}><AiOutlineUser /></li>
-          </ul>
-        </ul>
+        <Tabs>
+          <TabList>
+            <ul className={mypage["menu-list"]}>
+              <Tab>
+                <ul className={mypage["menu-list-in"]}>
+                  <li>新規投稿</li>
+                  <li className={mypage["menu-icon"]}><AiFillEdit /></li>
+                </ul>
+              </Tab>
+              <Tab>
+                <ul className={mypage["menu-list-in"]}>
+                  <li>お気に入り</li>
+                  <li className={mypage["menu-icon"]}><AiFillHeart /></li>
+                </ul>
+              </Tab>
+              <Tab>
+                <ul className={mypage["menu-list-in"]}>
+                  <li>投稿一覧</li>
+                  <li className={mypage["menu-icon"]}><AiFillFileText /></li>
+                </ul>
+              </Tab>
+              <Tab>
+                <ul className={mypage["menu-list-in-end"]}>
+                  <li>プロフィール</li>
+                  <li className={mypage["menu-icon"]}><AiOutlineUser /></li>
+                </ul>
+              </Tab>
+            </ul>
+          </TabList>
+
+          <TabPanel>
+            <div>新規投稿です</div>
+          </TabPanel>
+          <TabPanel>
+            <div>お気に入りです</div>
+          </TabPanel>
+          <TabPanel>
+            <div>投稿一覧です</div>
+          </TabPanel>
+          <TabPanel>
+            <Profile />
+          </TabPanel>
+        </Tabs>
       </div>
-      <Profile />
     </div>
   )
 }
