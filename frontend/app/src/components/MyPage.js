6b2f9abe35f91d532a52042944b1be7fc35369d2
yuki178504 <aerosmit1989x@gmail.com>
2022-10-14 16:45:08 +0900

    [add update]マイページデザイン枠組み実装 全体の背景色変更

--- frontend/app/src/components/MyPage.js
+++ frontend/app/src/components/MyPage.js
@@ -1,36 +1,34 @@
-import React, { useContext,useEffect } from "react";
-import { AuthContext } from "../route/Routers";
-import { getCurrentUser } from "../api/auth";
+import React from "react";
+import mypage from "../css/mypage.module.css"
+import { AiFillHome, AiFillEdit, AiFillHeart, AiFillFileText, AiOutlineUser } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const MyPage = () => {
-  const { setIsSignedIn, setLoading, currentUser, setCurrentUser } = useContext(AuthContext);
-
-  const handleGetCurrentUser = async () => {
-    try {
-      const res = await getCurrentUser();
-
-      if (res?.data.isLogin === true) {
-        setIsSignedIn(true);
-        setCurrentUser(res?.data.data);
-        console.log(res?.data.data);
-      } else {
-        console.log("ログインしていません");
-      }
-    } catch (e) {
-      console.log(e);
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    handleGetCurrentUser();
-  }, [setCurrentUser]);
-
   return (
-    <div>
-      <div>メールアドレス { currentUser.email }</div>
-      <div>ユーザー名 { currentUser.name }</div>
-
+    <div className={mypage.wrapper}>
+      <div className={mypage.content}>
+        <div className={mypage.title}><span className={mypage.home}><Link to='/' className={mypage["home-link"]}><span className={mypage["react-icons"]}><AiFillHome /></span>ホーム</Link></span> / マイページ</div>
+      </div>
+      <div className={mypage.menu}>
+        <ul className={mypage["menu-list"]}>
+          <ul className={mypage["menu-list-in"]}>
+            <li>新規投稿</li>
+            <li className={mypage["menu-icon"]}><AiFillEdit /></li>
+          </ul>
+          <ul className={mypage["menu-list-in"]}>
+            <li>お気に入り</li>
+            <li className={mypage["menu-icon"]}><AiFillHeart /></li>
+          </ul>
+          <ul className={mypage["menu-list-in"]}>
+            <li>投稿一覧</li>
+            <li className={mypage["menu-icon"]}><AiFillFileText /></li>
+          </ul>
+          <ul className={mypage["menu-list-in-end"]}>
+            <li>プロフィール</li>
+            <li className={mypage["menu-icon"]}><AiOutlineUser /></li>
+          </ul>
+        </ul>
+      </div>
     </div>
   )
 }
