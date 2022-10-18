73f198d80127b7b877c71547247cc06b809acb50
yuki178504 <aerosmit1989x@gmail.com>
2022-10-18 20:21:13 +0900

    [fix]index.jsのスペルミスを修正

--- frontend/app/src/components/Header.js
+++ frontend/app/src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import header from '../css/header.module.css';
 import { Link } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
   return <nav className={header["navbar"]}>
   <h4 className={header["navbar-logo"]}>
     <p><Link to='/' className={header["navbar-logo"]}>MangaBestGram</Link></p>
@@ -13,10 +16,19 @@ const Header = () => {
         <Link to="/" className={header["nav-li-text"]}>ゲストログイン</Link>
       </li>
       <li className={header["nav-li"]}>
-        <Link to="/signup" className={header["nav-li-text"]}>新規登録</Link>
+        <button onClick={() => loginWithRedirect()} className={header["nav-li-text"]}>新規登録</button>
       </li>
       <li className={header["nav-li"]}>
-        <Link to="/signin" className={header["nav-li-text"]}>ログイン</Link>
+        <button onClick={() => loginWithRedirect()} className={header["nav-li-text"]}>ログイン</button>
+      </li>
+      <li className={header["nav-li"]}>
+        <button onClick={() => logout()} className={header["nav-li-text"]}>ログアウト</button>
+      {
+        isAuthenticated ?
+        <p>ログイン</p>
+        :
+        <p>ログアウト</p>
+      }
       </li>
     </ul>
   </div>
--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    redirectUrl={window.location.origin}
+    redirectUri={window.location.origin}
   >
     <App />
   </Auth0Provider>,
