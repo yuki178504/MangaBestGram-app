647236f41a583900d8497fab9c541abc87eb91b0
yuki178504 <aerosmit1989x@gmail.com>
2022-10-21 03:23:52 +0900

    [update]Auth0の各ファイルにコメント追加 gitignoreにDS.store追加

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,24 @@
 class User < ApplicationRecord
   has_many :scene_posts, dependent: :destroy
+  #現在のユーザーを取得する
+  def self.current_user_from_token_payload(payload)
+    find_by(sub: payload['sub'])
+  end
+  #ユーザーを作成する
+  def self.create_user_from_token_payload(payload, name, introduction, image, url)
+    user = find_by(sub: payload['sub'])
+    if user
+      user.update(name: name, introduction: introduction, image: image, url: url)
+    else
+      ActiveRecord::Base.transaction do
+        create_user(payload['sub'], name, introduction, image, url)
+      end
+    end
+  end
 
-  def self.from_token_payload(payload)
-    find_by(sub: payload['sub']) || create!(sub: payload['sub'])
+  def self.create_user(sub, name, introduction, image, url)
+    params = URI.encode_www_form([%w[grant_type client_credentials], ['client_id', ENV['AUTH0_API_CLIENT_ID']],
+      ['client_secret', ENV['AUTH0_API_CLIENT_SECRET']], ['audience', ENV['AUTH0_AUDIENCE']]])
+
   end
 end
--- backend/app/services/authorization_service.rb
+++ backend/app/services/authorization_service.rb
@@ -9,7 +9,12 @@ class AuthorizationService
     #@auth_payloadにverify_tokenを代入している。@auth_headerはnilになる。
     @auth_payload, @auth_header = verify_token
     #@userに
-    @user = User.from_token_payload(@auth_payload)
+    @user = User.current_user_from_token_payload(@auth_payload)
+  end
+  #ユーザーモデルを実行し、ユーザーを作成するメソッド
+  def create_user(name, introduction, image, url)
+    @auth_payload, @auth_header = verify_token
+    @user = User.from_token_payload(@auth_payload, name, introduction, image, url)
   end
 
   private
