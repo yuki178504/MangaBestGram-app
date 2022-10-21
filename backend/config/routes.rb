dd5ccc3b84c985cf94e9f780c41e2647fd261646
yuki178504 <aerosmit1989x@gmail.com>
2022-10-22 05:31:23 +0900

    [remove update]不要コントローラ削除 comicsのルーティング追加
    テスト用の不要なtest_controller.rbを削除しました。comicsのルーティングを追加しました。

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :test, only: %i[index]
       resources :scene_posts
       resources :users
+      resources :comics
     end
   end
 end
