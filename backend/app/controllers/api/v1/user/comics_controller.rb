5b291b03a7ea5a464a5bb91fcb799dbc48ee14b4
yuki178504 <aerosmit1989x@gmail.com>
2022-10-22 05:25:52 +0900

    [add]userモデルにhas_many追加 comics_controller.rb追加

---
+++ backend/app/controllers/api/v1/user/comics_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::User::ComicsController < SecuredController
+
+  def index
+    comics = Comic.all
+    render json: comics
+  end
+
+  def show
+    comic = Comic.find(params[:id])
+    render json: comic
+  end
+
+  def create
+    comic = @current_user.comics.build(comic_params)
+    if comic.save
+      render json: comic
+    else
+      render json: comic.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    comic = Comic.find(params[:id])
+    if comic.update(comic_params)
+      render json: comic
+    else
+      render json: comic.errors
+    end
+  end
+
+  def destroy
+    comic = Comic.find(params[:id])
+    comic.delete
+  end
+
+  private
+
+  def comic_params
+    params.permit(:title, :genre, :image)
+  end
+end
