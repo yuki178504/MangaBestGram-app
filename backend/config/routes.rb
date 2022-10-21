ecbd55def2de89fc47ea0d48d208fdfb9afc7f7d
yuki178504 <aerosmit1989x@gmail.com>
2022-10-22 06:15:26 +0900

    [update]comicsのルーティング変更

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :scene_posts
       resources :users
-      resources :comics
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      namespace :user do
+        resources :comics
+      end
     end
   end
 end
