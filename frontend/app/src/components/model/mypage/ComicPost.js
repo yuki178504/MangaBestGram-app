d65e0adc2900e892df04cae402f748912620232f
yuki178504 <aerosmit1989x@gmail.com>
2022-10-22 23:42:45 +0900

    [add update]全体の背景画像変更 投稿一覧画面の仮デザイン実装

--- frontend/app/src/components/model/mypage/ComicPost.js
+++ frontend/app/src/components/model/mypage/ComicPost.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { getComic } from '../../../api/comic';
 import { AuthContext } from '../../../providers/AuthGuard';
+import comicPost from '../../../css/model/comicPost.module.css'
+import { Link } from 'react-router-dom';
 
 const ComicPost = () => {
   const { token, getToken } = useContext(AuthContext);
@@ -30,13 +32,26 @@ const ComicPost = () => {
   };
 
   return (
-    <div>
-    <div>コミックの投稿一覧の画面</div>
-    <div>
-    { comic.map((item) => (
-          <div>{item.title}</div>
-      )) }
-    </div>
+    <div className={comicPost.wrapper}>
+      <div className={comicPost["main-content"]}>
+        { comic.map((item) => (
+          <div className={comicPost.content}>
+            <div className={comicPost["innner-content"]}>
+              <div className={comicPost["outer-image"]}>
+                <img className={comicPost.image} src='' alt='画像' />
+              </div>
+              <div className={comicPost.list}>
+                <p className={comicPost["list-title"]}>{ item.title }</p>
+                <p className={comicPost["list-genre"]}>{ item.genre }</p>
+              </div>
+              <div className={comicPost["link-list"]}>
+              <Link to="/" className={comicPost["link-show"]} >シーンを見る 追加する</Link>
+              <Link to="/" className={comicPost["link-edit"]} >編集する</Link>
+              </div>
+            </div>
+          </div>
+          )) }
+      </div>
     </div>
   );
 };
