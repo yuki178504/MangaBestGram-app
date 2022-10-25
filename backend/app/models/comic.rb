6d582ab2619a36fd958a284c8e7a7ccec0ac2e2a
yuki178504 <aerosmit1989x@gmail.com>
2022-10-25 22:08:36 +0900

    [update]comicモデルとscene_postモデルの関連付け追加

--- backend/app/models/comic.rb
+++ backend/app/models/comic.rb
@@ -1,3 +1,4 @@
 class Comic < ApplicationRecord
   belongs_to :user
+  has_many :comics, dependent: :destroy
 end
--- backend/app/models/scene_post.rb
+++ backend/app/models/scene_post.rb
@@ -1,3 +1,4 @@
 class ScenePost < ApplicationRecord
+  belongs_to :comic
   belongs_to :user
 end
