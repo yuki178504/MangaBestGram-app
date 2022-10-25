3788b7b5c9a2a124e777c9878accf2fffc7f0d38
yuki178504 <aerosmit1989x@gmail.com>
2022-10-25 20:12:10 +0900

    [add]scene_postモデルにcomic_idカラムを追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_21_182104) do
+ActiveRecord::Schema.define(version: 2022_10_25_090012) do
 
   create_table "comics", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2022_10_21_182104) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.bigint "comic_id", null: false
+    t.index ["comic_id"], name: "index_scene_posts_on_comic_id"
     t.index ["user_id"], name: "index_scene_posts_on_user_id"
   end
 
