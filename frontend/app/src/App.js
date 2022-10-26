ad39c1441bc0dada30412ec3af17c541b2949234
yuki178504 <aerosmit1989x@gmail.com>
2022-10-26 17:25:45 +0900

    [add]react-queryを使用するための設定ファイル追加

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,23 +1,26 @@
-import React from 'react'
 import './css/App.css';
 import Routers from './route/Routers';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { AuthGuardProvider } from './providers/AuthGuard';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from './lib/queryClient';
 
 const App = () => {
   return (
-    <Router>
-      <AuthGuardProvider>
-        <Header />
-        <div className='main'>
-          <Routers />
-        </div>
-        <Footer />
-      </AuthGuardProvider>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AuthGuardProvider>
+          <Header />
+          <div className='main'>
+            <Routers />
+          </div>
+          <Footer />
+        </AuthGuardProvider>
+      </Router>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
