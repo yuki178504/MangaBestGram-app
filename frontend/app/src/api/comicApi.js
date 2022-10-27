1cef8aef3a963909cbd8ec8f8a4aa656bef30486
yuki178504 <aerosmit1989x@gmail.com>
2022-10-28 04:01:53 +0900

    [add update]react-hook-form導入 comicのcreateアクションのインスタンスを作成
    フォーム画面を簡略化するためにreact-hook-form導入

--- frontend/app/src/api/comicApi.js
+++ frontend/app/src/api/comicApi.js
@@ -13,5 +13,18 @@ export const comicApi = {
       console.error(error.res.data);
     });
     return res.data;
-  }
+  },
+  createComic: async (params, token) => {
+    const res = await axios
+    .post(`${process.env.REACT_APP_DEV_API_URL}/user/comics`, params, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    .catch((error) => {
+      console.error(error.res.data);
+    });
+    return res.data;
+  },
 };
