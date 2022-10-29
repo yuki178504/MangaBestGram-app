287428346e415f32d664ea754c18c8b1501d61ab
yuki178504 <aerosmit1989x@gmail.com>
2022-10-29 17:22:01 +0900

    [add update]シーンの新規投稿のapiのインスタンスを作成 カスタムフックを作成

--- frontend/app/src/api/scenePost.js
+++ frontend/app/src/api/scenePost.js
@@ -14,4 +14,17 @@ export const scenePost = {
     });
     return res.data;
   },
+
+  createScenePost: async (params, comicId, token) => {
+    await axios
+    .post(`${process.env.REACT_APP_DEV_API_URL}/user/comics/${comicId}/scene_posts`, params, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    .catch((error) => {
+      console.error(error.response.data);
+    });
+  }
 };
--- frontend/app/src/hooks/useScenePost.js
+++ frontend/app/src/hooks/useScenePost.js
@@ -23,5 +23,49 @@ export const useScenePost = () => {
       cacheTime: 30000000,
     });
   };
-  return { useGetScenePost }
+
+  //シーンの新規投稿
+  const useCreateScenePost = (comicId) => {
+    const queryClient = useQueryClient();
+    const queryKey = [
+      'scene_post',
+      { comicId: comicId },
+    ];
+
+    const updater = (previousData, data) => {
+      previousData.data.unshift({
+        attributes: data.comic,
+      });
+      return previousData;
+    };
+
+    return useMutation(
+      async (params) => {
+        return await scenePost.createScenePost(
+          params,
+          comicId,
+          token
+        );
+      },
+      {
+        onMutate: async (params) => {
+          await queryClient.cancelQueryData(queryKey);
+          const previousData = await queryClient.getQueryData(queryKey);
+
+          if (previousData) {
+            queryClient.setQueryData(queryKey, () => {
+              return updater(previousData, params);
+            });
+          }
+          return previousData;
+        },
+        onError: (err, _, context) => {
+          queryClient.setQueryData(queryKey, context);
+
+          console.warn(err);
+        },
+      }
+    );
+  };
+  return { useGetScenePost, useCreateScenePost }
 };
