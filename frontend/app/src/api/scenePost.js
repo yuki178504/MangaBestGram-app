5e64160033bc6a4c317591047c25843d76b4c781
yuki178504 <aerosmit1989x@gmail.com>
2022-11-03 04:20:07 +0900

    [add]scene_postのapiをまとめたオブジェクト群にshowオブジェクト追加

--- frontend/app/src/api/scenePost.js
+++ frontend/app/src/api/scenePost.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const scenePost = {
+  //取得用オブジェクト
   getScenePost: async (comicId, token) => {
     const res = await axios
     .get(`${process.env.REACT_APP_DEV_API_URL}/user/comics/${comicId}/scene_posts`, {
@@ -15,6 +16,7 @@ export const scenePost = {
     return res.data;
   },
 
+  //新規作成用オブジェクト
   createScenePost: async (params, comicId, token) => {
     await axios
     .post(`${process.env.REACT_APP_DEV_API_URL}/user/comics/${comicId}/scene_posts`, params, {
@@ -26,5 +28,20 @@ export const scenePost = {
     .catch((error) => {
       console.error(error.response.data);
     });
+  },
+
+  //詳細用オブジェクト
+  showScenePost: async (scenePostId, token) => {
+    const res = await axios
+    .get(`${process.env.REACT_APP_DEV_API_URL}/user/scene_posts/${scenePostId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    .catch((error) => {
+      console.error(error.res.data);
+    });
+    return res.data
   }
 };
