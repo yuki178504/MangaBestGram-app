962d83d59dc8d3218c896a827165d7ebd59bddee
yuki178504 <aerosmit1989x@gmail.com>
2022-11-04 08:09:18 +0900

    [add update]scene_postの編集画面へのルーティング作成 編集画面のコンポーネント作成

---
+++ frontend/app/src/components/model/comic/ScenePostEdit.js
@@ -0,0 +1,8 @@
+
+const ScenePostEdit = () => {
+  return (
+    <div>編集ページです</div>
+  );
+};
+
+export default ScenePostEdit;
--- frontend/app/src/route/Routers.js
+++ frontend/app/src/route/Routers.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home, TermsOfService, PrivacyPolicy, Contact, MyPage, ProfileEdit, ScenePost, ScenePostNewForm, ComicEdit, ScenePostShow } from './Pages';
+import { Home, TermsOfService, PrivacyPolicy, Contact, MyPage, ProfileEdit, ScenePost, ScenePostNewForm, ComicEdit, ScenePostShow, ScenePostEdit } from './Pages';
 
 const Routers = () => {
   return (
@@ -13,6 +13,7 @@ const Routers = () => {
       <Route path='/comic/:comic_id/:comic_title' element={ <ScenePost /> } />
       <Route path='/comic/:comic_id/:comic_title/scene_post_new' element={ <ScenePostNewForm /> } />
       <Route path='/scene_post/:comic_title/:scene_post_id' element={ <ScenePostShow /> } />
+      <Route path='/scene_post/:comic_title/:scene_post_id/:scene_post_title' element={ <ScenePostEdit /> } />
       <Route path='/comic/:comic_id/:comic_title/comic_edit' element={ <ComicEdit /> } />
     </Routes>
   );
