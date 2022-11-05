3ab25eac770656288947aba49cf78892b83ffd38
yuki178504 <aerosmit1989x@gmail.com>
2022-11-06 08:41:03 +0900

    [add]s3に保存するための設定ファイル追加

---
+++ backend/config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = ENV['BUCKET']
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_ACCCES_SECRET_KEY'],
+      region: 'ap-northeast-1',
+      path_style: true
+    }
+end 
