081d00e8ff4b647ffc0603a46f114672fc5d6eaa
yuki178504 <aerosmit1989x@gmail.com>
2022-11-08 23:33:35 +0900

    [add update]トップ画面で最新の投稿一覧に画像が表示されるように実装 レイアウト調整

--- frontend/app/src/components/Home.js
+++ frontend/app/src/components/Home.js
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom';
 import comicPost from '../css/model/comicPost.module.css';
 import { useGeneralComic } from '../hooks/useGeneralComic';
 import ReactLoading from "react-loading";
+import noimage from "../image/default.png"
 
 const Home = () => {
   const { useGetGeneralComic } = useGeneralComic();
   const { data, isLoading } = useGetGeneralComic();
   const { isAuthenticated, loginWithRedirect } = useContext(AuthContext);
 
-  if(isLoading) return <ReactLoading type="spin" />
+  if(isLoading) return <ReactLoading type="spin" color='blue' />
   console.log(data)
 
   return (
@@ -32,25 +33,26 @@ const Home = () => {
           }
           </div>
         </div>
-        <div className={home.search}>
-          検索窓
-        </div>
         <div className={ home.desc }>
           <h4>MangaBestGramとは？</h4>
-          <p>自分が気に入った！感銘を受けた！などのマンガの名場面を記録できるサービスです！</p>
+          <p className={home["desc-text"]}>自分が気に入った！感銘を受けた！などのマンガの名場面を記録できるサービスです！</p>
+          <h4>あの場面を心だけでなく、記録としても残してみませんか？</h4>
         </div>
       </div>
-      <div className={home.secction}>
+      <div className={home.section}>
+        <div className={home["section-outer-title"]}>
+          <h3 className={home["section-title"]}>最新の投稿</h3>
+        </div>
         <div className={comicPost["main-content"]}>
           {data.map((comic) => (
             <div key={comic.id} className={comicPost.content}>
               <div className={comicPost["innner-content"]}>
                 <div className={comicPost["outer-image"]}>
-                  <img className={comicPost.image} src='' alt='画像' />
+                  <img className={comicPost.image} src={ comic.image.url } alt='画像' onError={(e) => e.target.src = noimage} />
                 </div>
                 <div className={comicPost.list}>
-                  <p className={comicPost["list-title"]}>{ comic.title }</p>
-                  <p className={comicPost["list-genre"]}>{ comic.genre }</p>
+                  <p className={comicPost["list-title"]}>【{ comic.title }】</p>
+                  <p className={comicPost["list-genre"]}>【{ comic.genre }】</p>
                 </div>
                 <div className={comicPost["link-list"]}>
                   <Link to="/" className={comicPost["link-show"]} >シーンを見る</Link>
