0615e5735f1b7ebe5beaf25f99191cc2e2e4809f
yuki178504 <aerosmit1989x@gmail.com>
2022-11-09 17:45:17 +0900

    [add update]scene_postの一覧ページに、画像表示のロジックを追加 一部レイアウト修正

--- frontend/app/src/components/model/comic/ScenePost.js
+++ frontend/app/src/components/model/comic/ScenePost.js
@@ -1,9 +1,9 @@
-import React from 'react';
 import { useScenePost } from '../../../hooks/useScenePost';
 import ReactLoading from "react-loading";
 import { Link, useParams } from 'react-router-dom';
 import scenePost from '../../../css/model/comic/scenePost.module.css';
 import { AiFillHome } from "react-icons/ai";
+import noimage from "../../../image/default.png";
 
 const ScenePost = () => {
   const { comic_id, comic_title } = useParams();
@@ -11,7 +11,7 @@ const ScenePost = () => {
 
   const { data: scene_posts, isLoading } = useGetScenePost(comic_id);
 
-  if(isLoading) return <ReactLoading type="spin" />
+  if(isLoading) return <ReactLoading type="spin" color='blue' />
   console.log(scene_posts)
 
   return (
@@ -37,14 +37,14 @@ const ScenePost = () => {
           <div key={scene_post.id} className={scenePost.content}>
             <div className={scenePost["innner-content"]}>
               <div className={scenePost["outer-image"]}>
-                <img className={scenePost.image} src='' alt='画像' />
+                <img className={scenePost.image} src={ scene_post.scene_image.url } alt='画像' onError={(e) => e.target.src = noimage} />
               </div>
               <div className={scenePost.list}>
-                <p className={scenePost["list-title"]}>{ scene_post.scene_title }</p>
-                <p className={scenePost["list-genre"]}>{ scene_post.scene_content }</p>
+                <p className={scenePost["list-title"]}>【{ scene_post.scene_title }】</p>
+                <p className={scenePost["list-genre"]}>【{ scene_post.scene_content }】</p>
               </div>
               <div className={scenePost["link-list"]}>
-                <Link to={`/scene_post/${comic_title}/${scene_post.id}`} className={scenePost["link-show"]} >シーンを見る 追加する</Link>
+                <Link to={`/scene_post/${comic_title}/${scene_post.id}`} className={scenePost["link-show"]} >シーンを見る</Link>
                 <Link to={`/scene_post/${comic_id}/${scene_post.id}/scene_post_edit`} className={scenePost["link-edit"]} >編集する</Link>
               </div>
             </div>
