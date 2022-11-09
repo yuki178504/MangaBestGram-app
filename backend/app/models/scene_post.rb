249507bce2eb22dff6eab0cb67f4daab0b0d0433
yuki178504 <aerosmit1989x@gmail.com>
2022-11-09 15:40:35 +0900

    [add update]scene_postの新規投稿に画像を送信する処理を追加 Railsのscene_postモデルのscene_imageにcarrierwaveをマウント

--- backend/app/models/scene_post.rb
+++ backend/app/models/scene_post.rb
@@ -1,6 +1,8 @@
 class ScenePost < ApplicationRecord
   has_many :scene_post_images, dependent: :destroy
   accepts_nested_attributes_for :scene_post_images
+  mount_uploader :scene_image, FileUploader
+
 
   belongs_to :comic
   belongs_to :user
