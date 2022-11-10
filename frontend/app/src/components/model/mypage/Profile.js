e0cf4ac604604384fa8731866de568dc63db2f52
yuki178504 <aerosmit1989x@gmail.com>
2022-11-11 07:10:12 +0900

    [add]プロフィールページのレイアウト実装

--- frontend/app/src/components/model/mypage/Profile.js
+++ frontend/app/src/components/model/mypage/Profile.js
@@ -15,27 +15,41 @@ const Profile = () => {
 
   return (
     <div className={profile.wrapper}>
-      <div className={profile.content}>
-          <div>
-          <div className={profile.name}>{ user.name }</div>
-          <div className={profile.image}><img src={user.image.url} alt='画像' onError={(e) => e.target.src = noimage} /></div>
-          <div className={profile.introduction}>{ user.introduction }</div>
-          <div className={profile.url}>
-            { user.url }
+      <div className={profile.section}>
+        <div className={profile.content}>
+          <div className={profile['detail-area']}>
+            <h3 className={profile.detail}>プロフィール</h3>
           </div>
-          <div className={profile.edit}>
-            <button className={profile.button}><Link to={`/profile_edit/${user.id}`}>プロフィールを編集する</Link></button>
+          <div className={profile['detail-area']}>
+            <p className={profile.detail}>【ユーザー名】</p>
+            <div>{ user.name }</div>
           </div>
-        <ul className={profile.list}>
-          <ul className={profile.posts}>
-            <li>投稿数</li>
-            <li>0</li>
+          <div className={profile['outer-image']}>
+            <img className={profile.image} src={user.image.url} alt='画像' onError={(e) => e.target.src = noimage} />
+          </div>
+          <div className={profile['detail-area']}>
+            <p className={profile.detail}>【自己紹介】</p>
+            <div>{ user.introduction }</div>
+          </div>
+          <div className={profile['detail-area-last']}>
+            <p className={profile.detail}>【webサイトリンク】</p>
+            {reactStringReplace(user.url, regExp, (match, i) => (
+              <a className={profile.a} key={i} href={match}>{match}</a>
+            ))}
+          </div>
+          <div className={profile['detail-area']}>
+            <Link to={`/profile_edit/${user.id}`} className={profile.button}>プロフィールを編集する</Link>
+          </div>
+          <ul className={profile.list}>
+            <ul className={profile.posts}>
+              <li>【投稿数】</li>
+              <li>0</li>
+            </ul>
+            <ul className={profile.posts}>
+              <li>【お気に入り数】</li>
+              <li>0</li>
+            </ul>
           </ul>
-          <ul className={profile.posts}>
-            <li>お気に入り数</li>
-            <li>0</li>
-          </ul>
-        </ul>
         </div>
       </div>
     </div>
