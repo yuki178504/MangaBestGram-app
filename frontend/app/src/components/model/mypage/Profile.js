52b92d92f2d0bb9df9d33ea62d475ccc78e9b37e
yuki178504 <aerosmit1989x@gmail.com>
2022-11-11 02:38:29 +0900

    [add]プロフィール編集ページで編集機能のロジック実装

--- frontend/app/src/components/model/mypage/Profile.js
+++ frontend/app/src/components/model/mypage/Profile.js
@@ -7,23 +7,24 @@ import noimage from "../../../image/default.png";
 
 const Profile = () => {
   const { useGetUser } = useUser();
-  const { data: users, isLoading } = useGetUser();
+  const { data: user, isLoading } = useGetUser();
   const regExp = /(https?:\/\/\S+)/g;
 
   if(isLoading) return <ReactLoading type="spin" color='blue' />
-  console.log(users)
+  console.log(user)
 
   return (
     <div className={profile.wrapper}>
       <div className={profile.content}>
           <div>
-          <div className={profile.name}></div>
-          <div className={profile.image}><img src={users.image.url} alt='画像' onError={(e) => e.target.src = noimage} /></div>
-          <div className={profile.introduction}>自己紹介</div>
+          <div className={profile.name}>{ user.name }</div>
+          <div className={profile.image}><img src={user.image.url} alt='画像' onError={(e) => e.target.src = noimage} /></div>
+          <div className={profile.introduction}>{ user.introduction }</div>
           <div className={profile.url}>
+            { user.url }
           </div>
           <div className={profile.edit}>
-            <button className={profile.button}><Link to={`/profile_edit/${users.id}`}>プロフィールを編集する</Link></button>
+            <button className={profile.button}><Link to={`/profile_edit/${user.id}`}>プロフィールを編集する</Link></button>
           </div>
         <ul className={profile.list}>
           <ul className={profile.posts}>
--- frontend/app/src/components/model/profile/ProfileEdit.js
+++ frontend/app/src/components/model/profile/ProfileEdit.js
@@ -1,8 +1,108 @@
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import { useUser } from "../../../hooks/useUser";
+import { AuthContext } from "../../../providers/AuthGuard";
+import newComicForm from "../../../css/ui/newComicForm.module.css";
+import { AiFillHome } from "react-icons/ai";
+import axios from "axios";
+import ReactLoading from "react-loading";
+import comicEdit from "../../../css/ui/comicEdit.module.css";
 
 const ProfileEdit = () => {
-  return (
-    <div>あああ</div>
+  const navigate = useNavigate();
+  const { user_id } = useParams();
+  const { token } = useContext(AuthContext);
 
+  const { useGetUser } = useUser();
+  const { data: user, isLoading } = useGetUser(user_id);
+
+  //更新関数
+  const onSubmit = async (data) => {
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("introduction", data.introduction);
+    formData.append("url", data.url);
+    formData.append("image", data.image[0]);
+
+    await axios.put(`${process.env.REACT_APP_DEV_API_URL}/user/users/${user_id}`, formData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+    .catch((error) => {
+      console.error(error.res.data);
+    });
+    alert(`編集しました！`);
+    navigate("/mypage");
+    console.log(data)
+  };
+
+  const { handleSubmit, register, formState: { errors } } = useForm({
+    criteriaMode: "all"
+  });
+
+  if(isLoading) return <ReactLoading type="spin" color="blue" />
+  console.log(user)
+
+  return (
+    <>
+    <div className={comicEdit["top-list"]}>
+        <div className={comicEdit.title}>
+          <span className={comicEdit.home}>
+            <Link to='/' className={comicEdit["home-link"]}><span className={comicEdit["react-icons"]}><AiFillHome /></span>ホーム</Link>
+          </span>
+          <span>
+            <Link to='/mypage' className={comicEdit["home-link"]}><span>/ マイページ</span></Link>
+          </span>
+          <span>/ プロフィール編集</span>
+        </div>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)} className={newComicForm.form}>
+        <div className={newComicForm["form-text"]}>
+          <div className={newComicForm["form-label"]}>ユーザー名</div>
+          { errors.name &&
+            <div className={newComicForm.errors}>【！ユーザー名がありません】</div> 
+          }
+          <input
+            className={newComicForm["form-input"]}
+            defaultValue={ user.name }
+            {...register('name', {
+              required: true
+            })}
+          />
+        </div>
+        <div className={newComicForm["form-text"]}>
+          <div className={newComicForm["form-label"]}>自己紹介</div>
+          <input
+            className={newComicForm["form-input"]}
+            defaultValue={ user.introduction }
+            {...register('introduction')}
+          />
+        </div>
+        <div className={newComicForm["form-text"]}>
+          <div className={newComicForm["form-label"]}>リンク</div>
+          <input
+            className={newComicForm["form-input"]}
+            defaultValue={ user.url }
+            {...register('url')}
+          />
+        </div>
+        <div className={newComicForm["form-text"]}>
+          <div className={newComicForm["form-label"]}>ユーザーアイコン</div>
+          <input
+            className={newComicForm["form-input"]}
+            type="file"
+            accept="image/*"
+            {...register("image")}
+          />
+        </div>
+        <div className={newComicForm["form-text"]}>
+          <button className={newComicForm["form-submit"]} type="submit">この内容で登録する</button>
+        </div>
+      </form>
+    </>
   );
 };
 
