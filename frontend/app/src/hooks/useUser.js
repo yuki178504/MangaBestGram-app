8022deedbc5b91f9c14ee75404b69a6f2ae5075d
yuki178504 <aerosmit1989x@gmail.com>
2022-11-10 23:32:02 +0900

    [add]ユーザー情報を取得するカスタムフックを作成

---
+++ frontend/app/src/hooks/useUser.js
@@ -0,0 +1,18 @@
+import { useContext } from "react"
+import { useQuery } from "react-query";
+import { user } from "../api/user";
+import { AuthContext } from "../providers/AuthGuard"
+
+export const useUser = () => {
+  const { token } = useContext(AuthContext);
+  //取得用関数
+  const useGetUser = () => {
+    return useQuery({
+      queryKey: 'user',
+      queryFn: () => user.getUser(token),
+      staleTime: 30000000,
+      cacheTime: 0,
+    });
+  };
+  return { useGetUser };
+};
