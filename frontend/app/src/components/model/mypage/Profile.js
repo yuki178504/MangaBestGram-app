9031c86c83fae7b215f984bfde271e8edbf8a2f2
yuki178504 <aerosmit1989x@gmail.com>
2022-11-11 01:13:00 +0900

    [add]プロフィール画面にて、ユーザー情報が表示されるように実装

--- frontend/app/src/components/model/mypage/Profile.js
+++ frontend/app/src/components/model/mypage/Profile.js
@@ -1,54 +1,44 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../../../providers/AuthGuard';
 import profile from '../../../css/model/profile.module.css';
 import { Link } from 'react-router-dom';
 import reactStringReplace from "react-string-replace";
-//import { getList } from '../../../api/auth';
+import { useUser } from '../../../hooks/useUser';
+import ReactLoading from "react-loading";
+import noimage from "../../../image/default.png";
 
 const Profile = () => {
-  const { user, isAuthenticated } = useContext(AuthContext);
-  const [dataList, setDataList] = useState([]);
+  const { useGetUser } = useUser();
+  const { data: users, isLoading } = useGetUser();
   const regExp = /(https?:\/\/\S+)/g;
 
-  useEffect(() => {
-    console.log(user)
-  }, []);
-
-  // const handleGetList = async () => {
-  //   try {
-  //     const res = await getList();
-  //     console.log(res.data);
-  //     setDataList(res.data);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
+  if(isLoading) return <ReactLoading type="spin" color='blue' />
+  console.log(users)
 
   return (
     <div className={profile.wrapper}>
       <div className={profile.content}>
-        <div className={profile.name}>{ user.name }</div>
-        <div className={profile.image}><img src={user.picture} /></div>
-        <div className={profile.introduction}>自己紹介</div>
-        <div className={profile.url}>
-          
-        </div>
-        <div className={profile.edit}>
-          <button className={profile.button}><Link to={`/`}>プロフィールを編集する</Link></button>
-        </div>
-      <ul className={profile.list}>
-        <ul className={profile.posts}>
-          <li>投稿数</li>
-          <li>0</li>
+          <div>
+          <div className={profile.name}></div>
+          <div className={profile.image}><img src={users.image.url} alt='画像' onError={(e) => e.target.src = noimage} /></div>
+          <div className={profile.introduction}>自己紹介</div>
+          <div className={profile.url}>
+          </div>
+          <div className={profile.edit}>
+            <button className={profile.button}><Link to={`/`}>プロフィールを編集する</Link></button>
+          </div>
+        <ul className={profile.list}>
+          <ul className={profile.posts}>
+            <li>投稿数</li>
+            <li>0</li>
+          </ul>
+          <ul className={profile.posts}>
+            <li>お気に入り数</li>
+            <li>0</li>
+          </ul>
         </ul>
-        <ul className={profile.posts}>
-          <li>お気に入り数</li>
-          <li>0</li>
-        </ul>
-      </ul>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Profile;
