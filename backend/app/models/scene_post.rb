681b002f94b6e862133b1c05debc16d3e321c69c
yuki178504 <aerosmit1989x@gmail.com>
2022-11-10 21:40:31 +0900

    [remove]scene_post_imagesテーブルを不要になったため削除 それに伴って、関連ファイル、コードを削除

--- backend/app/models/scene_post.rb
+++ backend/app/models/scene_post.rb
@@ -1,8 +1,5 @@
 class ScenePost < ApplicationRecord
-  has_many :scene_post_images, dependent: :destroy
-  accepts_nested_attributes_for :scene_post_images
   mount_uploader :scene_image, FileUploader
-
 
   belongs_to :comic
   belongs_to :user
--- backend/app/models/scene_post_image.rb
+++
@@ -1,4 +0,0 @@
-class ScenePostImage < ApplicationRecord
-  belongs_to :scene_post
-  mount_uploader :scene_post_image_url, FileUploader
-end
