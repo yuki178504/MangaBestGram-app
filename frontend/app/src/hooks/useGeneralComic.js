a85cd8c8d5283e8f5802528bef15ff82b6257a62
yuki178504 <aerosmit1989x@gmail.com>
2022-11-11 14:24:54 +0900

    [add]全ユーザーが閲覧できる投稿の詳細のカスタムフック作成

--- frontend/app/src/hooks/useGeneralComic.js
+++ frontend/app/src/hooks/useGeneralComic.js
@@ -1,7 +1,6 @@
 import { useQuery } from 'react-query';
 import { generalComic } from '../api/generalComic';
 
-
 export const useGeneralComic = () => {
   //全ユーザーが閲覧できる投稿一覧をuseQueryで定義
   const useGetGeneralComic = () => {
@@ -12,5 +11,21 @@ export const useGeneralComic = () => {
       cacheTime: 0,
     });
   };
-  return { useGetGeneralComic };
+
+  //全ユーザーが閲覧できる投稿の詳細
+  const useShowGeneralComic = (comicId) => {
+    return useQuery({
+      queryKey: [
+        'general_comic_show',
+        { comicId: comicId }
+      ],
+      queryFn: () => generalComic.showGeneralComic(
+        comicId
+      ),
+      enabled: !!comicId,
+      staleTime: 30000000,
+      cacheTime: 0,
+    });
+  };
+  return { useGetGeneralComic, useShowGeneralComic };
 };
