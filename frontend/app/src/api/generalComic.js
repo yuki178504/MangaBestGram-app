291a398605a3952b38280fc9d5e1d1d9b1916ebc
yuki178504 <aerosmit1989x@gmail.com>
2022-11-12 01:01:11 +0900

    [update]全ユーザーが閲覧できるcomicの一覧のカスタムフックとオブジェクト作成

--- frontend/app/src/api/generalComic.js
+++ frontend/app/src/api/generalComic.js
@@ -1,10 +1,19 @@
 import instance from "./instance";
 
 export const generalComic = {
-  //全ユーザーが閲覧できる投稿一覧を取得する
+  //全ユーザーが閲覧できる最新の投稿3つの投稿一覧を取得する
   getGeneralLatestComic: async () => {
     const res = await instance
     .get('/comics/latest')
+    .catch((error) => {
+      console.error(error.res.data);
+    });
+    return res.data
+  },
+  //全ユーザーが閲覧できるの投稿一覧を取得する
+  getGeneralComic: async () => {
+    const res = await instance
+    .get('/comics')
     .catch((error) => {
       console.error(error.res.data);
     });
--- frontend/app/src/hooks/useGeneralComic.js
+++ frontend/app/src/hooks/useGeneralComic.js
@@ -2,11 +2,20 @@ import { useQuery } from 'react-query';
 import { generalComic } from '../api/generalComic';
 
 export const useGeneralComic = () => {
-  //全ユーザーが閲覧できる投稿一覧をuseQueryで定義
+  //全ユーザーが閲覧できる最新の投稿３つの投稿一覧をuseQueryで定義
   const useGetGeneralLatestComic = () => {
     return useQuery({
-      queryKey: 'general_comic',
+      queryKey: 'general_latest_comic',
       queryFn: () => generalComic.getGeneralLatestComic(),
+      staleTime: 300000,
+      cacheTime: 0,
+    });
+  };
+
+  const useGetGeneralComic = () => {
+    return useQuery({
+      queryKey: 'general_comic',
+      queryFn: () => generalComic.getGeneralComic(),
       staleTime: 300000,
       cacheTime: 0,
     });
@@ -27,5 +36,5 @@ export const useGeneralComic = () => {
       cacheTime: 0,
     });
   };
-  return { useGetGeneralLatestComic, useShowGeneralComic };
+  return { useGetGeneralLatestComic, useGetGeneralComic, useShowGeneralComic };
 };
