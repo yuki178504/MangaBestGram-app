6e1dc0bbf50d933e6b75097cbaaf88c52e4d2f7a
yuki178504 <aerosmit1989x@gmail.com>
2022-11-11 14:17:46 +0900

    [add]全ユーザーが閲覧できるcomicのオブジェクトを定義

--- frontend/app/src/api/generalComic.js
+++ frontend/app/src/api/generalComic.js
@@ -9,5 +9,14 @@ export const generalComic = {
       console.error(error.res.data);
     });
     return res.data
+  },
+  //全ユーザーが閲覧できる投稿詳細を取得
+  showGeneralComic: async (comicId) => {
+    const res = await instance
+    .get(`/comics/${comicId}`)
+    .catch((error) => {
+      console.error(error.res.data);
+    });
+    return res.data
   }
 };
