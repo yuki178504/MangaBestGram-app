399072ba65235d430379cd25dfaa2e7d373c23ab
yuki178504 <aerosmit1989x@gmail.com>
2022-11-12 17:36:21 +0900

    [add]  ユーザーが閲覧できるcomicのレイアウト実装 情報を取得して表示するロジック実装

--- frontend/app/src/components/model/general/GeneralComic.js
+++ frontend/app/src/components/model/general/GeneralComic.js
@@ -1,8 +1,9 @@
 import { useGeneralComic } from "../../../hooks/useGeneralComic";
 import ReactLoading from "react-loading";
 import { Link } from 'react-router-dom';
-import comicPost from '../../../css/model/comicPost.module.css';
 import noimage from "../../../image/default.png";
+import generalComic from "../../../css/model/general/generalComic.module.css";
+import { AiFillHome } from "react-icons/ai";
 
 const GeneralComic = () => {
   const { useGetGeneralComic } = useGeneralComic();
@@ -12,20 +13,27 @@ const GeneralComic = () => {
   console.log(comics)
 
   return (
-    <div className={comicPost.wrapper}>
-      <div className={comicPost["main-content"]}>
+    <div className={generalComic.wrapper}>
+      <div className={generalComic["top-list"]}>
+        <div className={generalComic.title}>
+          <span className={generalComic.home}>
+            <Link to='/' className={generalComic["home-link"]}><span className={generalComic["react-icons"]}><AiFillHome /></span>ホーム</Link>
+          </span>
+        </div>
+      </div>
+      <div className={generalComic["main-content"]}>
         {comics?.map((comic) => (
-          <div key={comic.id} className={comicPost.content}>
-            <div className={comicPost["innner-content"]}>
-              <div className={comicPost["outer-image"]}>
-                <img className={comicPost.image} src={ comic.image.url } alt='画像' onError={(e) => e.target.src = noimage} />
+          <div key={comic.id} className={generalComic.content}>
+            <div className={generalComic["innner-content"]}>
+              <div className={generalComic["outer-image"]}>
+                <img className={generalComic.image} src={ comic.image.url } alt='画像' onError={(e) => e.target.src = noimage} />
               </div>
-              <div className={comicPost.list}>
-                <p className={comicPost["list-title"]}>【{ comic.title }】</p>
-                <p className={comicPost["list-genre"]}>【{ comic.genre }】</p>
+              <div className={generalComic.list}>
+                <p className={generalComic["list-title"]}>【{ comic.title }】</p>
+                <p className={generalComic["list-genre"]}>【{ comic.genre }】</p>
               </div>
-              <div className={comicPost["link-list"]}>
-              <Link to={`/scene_post/${comic.id}`} className={comicPost["link-show"]} >シーンを見る</Link>
+              <div className={generalComic["link-list"]}>
+              <Link to={`/scene_post/${comic.id}`} className={generalComic["link-show"]} >シーンを見る</Link>
               </div>
             </div>
           </div>
