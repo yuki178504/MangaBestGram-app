3d546f239822ab07f57811961b7e40e1e61f757b
yuki178504 <aerosmit1989x@gmail.com>
2022-11-15 17:43:44 +0900

    [add]お気に入りの削除ボタンのコンポーネントを実装

---
+++ frontend/app/src/components/ui/UnFavoriteButton.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthGuard";
+
+const UnFavoriteButton = ({id}) => {
+  const { token } = useContext(AuthContext);
+
+  const handleDelete = () => {
+    axios.delete(`${process.env.REACT_APP_DEV_API_URL}/user/favorites/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    .catch((error) => {
+      console.error(error.res.data);
+    });
+  }
+
+  return (
+    <button type='button' onClick={handleDelete}>
+      お気に入り済み
+    </button>
+  );
+};
+
+export default UnFavoriteButton;
