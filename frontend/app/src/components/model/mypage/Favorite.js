a5acdbfde158cb36d7f0781554522c0d2ba0d2ef
yuki178504 <aerosmit1989x@gmail.com>
2022-11-19 17:18:24 +0900

    [add update]お気に入り一覧のレイアウト調整 cssモジュール追加

--- frontend/app/src/components/model/mypage/Favorite.js
+++ frontend/app/src/components/model/mypage/Favorite.js
@@ -2,7 +2,9 @@ import { useFavorite } from "../../../hooks/useFavorite";
 import ReactLoading from "react-loading";
 import { Link } from 'react-router-dom';
 import noimage from "../../../image/default.png";
-import scenePost from '../../../css/model/comic/scenePost.module.css';
+import generalScenePostCss from '../../../css/model/general/generalScenePostCss.module.css';
+import favorite from '../../../css/model/mypage/favorite.module.css';
+import { BsBookFill, BsJournalBookmarkFill } from "react-icons/bs";
 
 const Favorite = () => {
   const { useGetFavorite } = useFavorite();
@@ -12,23 +14,30 @@ const Favorite = () => {
   console.log(favorites)
 
   return (
-    <div className={scenePost.wrapper}>
-      <div className={scenePost["main-content"]}>
-        {favorites?.map((favorite) => (
-          <div key={favorite.id} className={scenePost.content}>
-            <div className={scenePost["innner-content"]}>
-              <div className={scenePost["outer-image"]}>
-                <img className={scenePost.image} src={ favorite.scene_image.url } alt='画像' onError={(e) => e.target.src = noimage} />
+    <div className={favorite.wrapper}>
+      <div className={favorite['main-content']}>
+        {favorites.data.map((favorite) => (
+          <div className={generalScenePostCss.content}>
+          <div className={generalScenePostCss["innner-content"]}>
+            <div className={generalScenePostCss.list}>
+              <div className={generalScenePostCss["user-name"]}><img className={generalScenePostCss["user-image"]} src={ favorite.attributes.scene_post_user_image } alt='画像' onError={(e) => e.target.src = noimage} />{ favorite.attributes.scene_post_user_name }</div>
+              <div className={generalScenePostCss["detail-area"]}>
+                <p className={generalScenePostCss.detail}><span className={generalScenePostCss["bs-book-fill"]}><BsBookFill /></span>【シーン名】</p>
+                <div>{ favorite.attributes.scene_title }</div>
               </div>
-              <div className={scenePost.list}>
-                <p className={scenePost["list-title"]}>【{ favorite.scene_title }】</p>
-                <p className={scenePost["list-genre"]}>【{ favorite.scene_content }】</p>
+              <div className={generalScenePostCss["detail-area"]}>
+                <p className={generalScenePostCss.detail}><span className={generalScenePostCss["bs-journal-book-mark-fill"]}><BsJournalBookmarkFill /></span>【シーンの日付】</p>
+                <div>{ favorite.attributes.scene_date }</div>
               </div>
-              <div className={scenePost["link-list"]}>
-                {/* <Link to={`/scene_post/${comic_title}/${favorite.id}`} className={scenePost["link-show"]} >シーンを見る</Link> */}
+              <div className={generalScenePostCss["detail-area-link"]}>
+                {/* <Link to={`/general_scene_post/${comicTitle}/general_scene_post_show/${scenePostId}`} className={generalScenePostCss["link-show"]} >シーンを見る</Link> */}
               </div>
             </div>
+            <div className={generalScenePostCss["outer-image"]}>
+              <img className={generalScenePostCss.image} src={ favorite.attributes.scene_image.url } alt='画像' onError={(e) => e.target.src = noimage} />
+            </div>
           </div>
+        </div>
           ))}
       </div>
     </div>
