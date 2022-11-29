17ef0d5694a1539e39fef7226f135301eed58c21
yuki178504 <aerosmit1989x@gmail.com>
2022-11-30 03:34:47 +0900

    [remove]不要ファイル削除

--- frontend/app/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import instance from "./instance";
-
-export const updateAccount = (id, params) => {
-  return instance.put(`/accounts/${id}`, params);
-}
-
-export const getDetail = (id) => {
-  return instance.get(`/accounts/${id}`);
-};
--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN || ''
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID || ''
