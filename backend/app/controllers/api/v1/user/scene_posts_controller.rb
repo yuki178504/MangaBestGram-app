ef4573f10bea152ae12107a21f917429ed90bf81
yuki178504 <aerosmit1989x@gmail.com>
2022-12-05 17:38:55 +0900

    [add]各コントローラ、シリアライザーにsub_titleカラムを追加

--- backend/app/controllers/api/v1/user/scene_posts_controller.rb
+++ backend/app/controllers/api/v1/user/scene_posts_controller.rb
@@ -35,10 +35,10 @@ class Api::V1::User::ScenePostsController < SecuredController
   end
 
   def scene_post_params
-    params.permit(:scene_title, :scene_date, :scene_content, :scene_image, :scene_number).merge(user_id: @current_user.id)
+    params.permit(:scene_title, :scene_date, :scene_content, :scene_image, :scene_number, :sub_title).merge(user_id: @current_user.id)
   end
 
   def scene_post_update_params
-    params.permit(:scene_title, :scene_date, :scene_content, :scene_image, :scene_number)
+    params.permit(:scene_title, :scene_date, :scene_content, :scene_image, :scene_number, :sub_title)
   end
 end
