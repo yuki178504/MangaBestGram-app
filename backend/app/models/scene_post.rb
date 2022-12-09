ace4444ebef36c490e5d2dfedfd70b2b5fa14789
yuki178504 <aerosmit1989x@gmail.com>
2022-12-09 21:41:06 +0900

    [add]userモデルとscene_postモデルにリレーションを追加

--- backend/app/models/scene_post.rb
+++ backend/app/models/scene_post.rb
@@ -3,6 +3,7 @@ class ScenePost < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  has_many :comments, dependent: :destroy
   belongs_to :comic
   belongs_to :user
 end
