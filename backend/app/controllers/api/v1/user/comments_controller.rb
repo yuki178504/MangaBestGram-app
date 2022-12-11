e19a8685bed1cd8593f19d9ca09b36c105846f9f
yuki178504 <aerosmit1989x@gmail.com>
2022-12-11 23:23:01 +0900

    [add update]バックエンドにcomment_serializerを作成 comments_controllerをシリアライザー形式に変更
    コメントの投稿にユーザー情報を表示させるためにユーザー情報を紐付けしました

--- backend/app/controllers/api/v1/user/comments_controller.rb
+++ backend/app/controllers/api/v1/user/comments_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::User::CommentsController < SecuredController
 
   def index
     comments = ScenePost.find_by!(id: params[:scene_post_id]).comments.all.order(id: :desc)
-    render json: comments
+    render_json = User::CommentSerializer.new(comments).serializable_hash.to_json
+    render json: render_json
   end
 
   def create
