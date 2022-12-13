1823596bb47a9ee2f366f027c45002c78891ba50
yuki178504 <aerosmit1989x@gmail.com>
2022-12-13 22:38:04 +0900

    [add]マイページのシーン詳細画面にコメントアイコン追加 css追加 レイアウト調整

--- frontend/app/src/components/model/scene_post/ScenePostShow.js
+++ frontend/app/src/components/model/scene_post/ScenePostShow.js
@@ -3,7 +3,7 @@ import { useScenePost } from "../../../hooks/useScenePost";
 import ReactLoading from "react-loading";
 import scenePostShow from "../../../css/model/scene_post/scenePostShow.module.css";
 import { AiFillHome } from "react-icons/ai";
-import { BsBookFill, BsFillReplyFill, BsFillPencilFill, BsCalendar3, BsNewspaper, BsFillJournalBookmarkFill, BsReceipt } from "react-icons/bs";
+import { BsBookFill, BsFillReplyFill, BsFillPencilFill, BsCalendar3, BsNewspaper, BsFillJournalBookmarkFill, BsReceipt, BsFillChatRightDotsFill } from "react-icons/bs";
 import noimage from "../../../image/default.png";
 import moment from 'moment';
 import Comment from "../comment/Comment";
@@ -64,7 +64,7 @@ const ScenePostShow = () => {
             </div>
             <div className={scenePostShow["create-at"]}><span className={scenePostShow["detail-text"]}><span className={scenePostShow["bs-calender-3"]}><BsCalendar3 /></span>{ moment(scene_post.created_at).format('YYYY年MM月DD日HH:mm') }</span></div>
             <div className={scenePostShow["detail-area-comment"]}>
-              <div className={scenePostShow["detail-comment"]}>【コメント一覧】</div>
+              <div className={scenePostShow["detail-comment"]}><span className={scenePostShow["bs-fill-chat-right-dots-fill"]}><BsFillChatRightDotsFill /></span>【コメント一覧】</div>
               <Comment scene_post_id={scene_post_id} />
             </div>
           </div>
