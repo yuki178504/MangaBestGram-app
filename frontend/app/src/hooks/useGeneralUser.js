e345bb06e6db6aaa52936d9edb9d9cb4110bd565
yuki178504 <aerosmit1989x@gmail.com>
2022-12-15 00:40:01 +0900

    [add]Userのカスタムフック作成

---
+++ frontend/app/src/hooks/useGeneralUser.js
@@ -0,0 +1,44 @@
+import { useQuery } from "react-query";
+import { generalUser } from "../api/generalUser";
+
+export const useGeneralUser = () => {
+  const useGetGeneralUser = () => {
+    return useQuery({
+      queryKey: 'general_user',
+      queryFn: () => generalUser.getGeneralUser(),
+      staleTime: 300000,
+      cacheTime: 0,
+    });
+  };
+
+  const useShowGeneralUser = (userId) => {
+    return useQuery({
+      queryKey: [
+        'general_user_show',
+        { userId: userId }
+      ],
+      queryFn: () => generalUser.showGeneralUser(
+        userId
+      ),
+      enabled: !!comicId,
+      staleTime: 30000000,
+      cacheTime: 0,
+    });
+  };
+
+  const useGetGeneralUserComic = (userId) => {
+    return useQuery({
+      queryKey: [
+        'general_user_comic',
+        { userId: userId }
+      ],
+      queryFn: () => generalUser.getGeneralUserComic(
+        userId
+      ),
+      staleTime: 30000000,
+      cacheTime: 0,
+    });
+  };
+
+  return { useGetGeneralUser, useShowGeneralUser, useGetGeneralUserComic };
+};
