bcef3b32dbad16ce602595b6f46df4d5c2a0fd48
yuki178504 <aerosmit1989x@gmail.com>
2022-12-15 00:26:36 +0900

    [add]フロントエンドのuserのapiオブジェクト作成

---
+++ frontend/app/src/api/generalUser.js
@@ -0,0 +1,30 @@
+import instance from "./instance"
+
+export const generalUser = {
+  getGeneralUser: async () => {
+    const res = await instance
+    .get('/users')
+    .catch((error) => {
+      console.error(error.res.data);
+    });
+    return res.data
+  },
+
+  showGeneralUser: async (userId) => {
+    const res = await instance
+    .get(`/users/${userId}`)
+    .catch((error) => {
+      console.error(error.res.data);
+    });
+    return res.data
+  },
+
+  getGeneralUserComic: async (userId) => {
+    const res = await instance
+    .get(`/users/${userId}/user_comics`)
+    .catch((error) => {
+      console.error(error.res.data);
+    });
+    return res.data
+  }
+};
