1c5a7ad92d301b4d6a52bf2b46aa5bac9a898a23
yuki178504 <aerosmit1989x@gmail.com>
2022-12-16 03:27:40 +0900

    [add]マイページのシーン一覧画面のアイコン変更 レイアウト調整

--- frontend/app/src/components/model/scene_post/ui/ScenePostCard.js
+++ frontend/app/src/components/model/scene_post/ui/ScenePostCard.js
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
 import scenePost from '../../../../css/model/scene_post/scenePost.module.css';
 import moment from 'moment';
-import { BsBookFill, BsJournalBookmarkFill, BsCalendar3, BsFillChatRightDotsFill } from "react-icons/bs";
 import noimage from "../../../../image/default.png";
-import ReactLoading from "react-loading";
 import { useGeneralComment } from "../../../../hooks/useGeneralComment";
+import { FcCalendar, FcMms, FcFilm, FcContacts, FcSms, FcEditImage } from "react-icons/fc";
 
 const ScenePostCard = ({
   scenePostId,
@@ -19,7 +18,7 @@ const ScenePostCard = ({
 
   const { data: generalComments, isLoading } = useGetGeneralComment(scenePostId);
 
-  if(isLoading) return <ReactLoading type="spin" color='blue' className='loading' />
+  if(isLoading) return <></>
 
   return (
     <div className={scenePost.content}>
@@ -29,22 +28,22 @@ const ScenePostCard = ({
         </div>
         <div className={scenePost.list}>
           <div className={scenePost["detail-area"]}>
-              <p className={scenePost.detail}><span className={scenePost["bs-book-fill"]}><BsBookFill /></span>【サブタイトル】</p>
+              <p className={scenePost.detail}><span className={scenePost["react-icon"]}><FcFilm /></span>サブタイトル</p>
               <div>{ scenePostSubTitle}</div>
           </div>
           <div className={scenePost["detail-area"]}>
-            <p className={scenePost.detail}><span className={scenePost["bs-journal-book-mark-fill"]}><BsJournalBookmarkFill /></span>【話数】</p>
+            <p className={scenePost.detail}><span className={scenePost["react-icon"]}><FcContacts /></span>話数</p>
             <div>{ scenePostNumber }話</div>
           </div>
         </div>
         <div className={scenePost["link-list"]}>
-          <Link to={`/scene_post/${comicTitle}/${scenePostId}`} className={scenePost["link-show"]} >シーンを見る</Link>
-          <Link to={`/scene_post/${comicId}/${comicTitle}/${scenePostId}/scene_post_edit`} className={scenePost["link-edit"]} >編集する</Link>
+          <Link to={`/scene_post/${comicTitle}/${scenePostId}`} className={scenePost["link-show"]} ><span className={scenePost["react-icon"]}><FcMms /></span>シーンを見る</Link>
+          <Link to={`/scene_post/${comicId}/${comicTitle}/${scenePostId}/scene_post_edit`} className={scenePost["link-edit"]}><span className={scenePost["react-icon"]}><FcEditImage /></span>編集する</Link>
         </div>
-        <div className={scenePost["create-at"]}><span className={scenePost["detail-text"]}><span className={scenePost["bs-calender-3"]}><BsCalendar3 /></span>{ moment(scenePostCreatedAt).format('YYYY年MM月DD日HH:mm') }</span></div>
+        <div className={scenePost["create-at"]}><span className={scenePost["detail-text"]}><span className={scenePost["react-icon"]}><FcCalendar /></span>{ moment(scenePostCreatedAt).format('YYYY年MM月DD日HH:mm') }</span></div>
         <div className={scenePost['detail-area-count']}>
           <div className={scenePost['detail-area-list']}>
-            <div><span className={scenePost["bs-fill-chat-right-dots-fill"]}><BsFillChatRightDotsFill /></span>コメント&nbsp;{ generalComments.data.length }件</div>
+            <div><span className={scenePost["react-icon"]}><FcSms /></span>コメント&nbsp;{ generalComments.data.length }件</div>
           </div>
         </div>
       </div>
