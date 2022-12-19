ba0faec014b134a4123368795aa503f5bc8bbdd9
yuki178504 <aerosmit1989x@gmail.com>
2022-12-19 12:11:43 +0900

    [add]マイページのComic編集画面の削除ボタンの削除確認画面を実装 確認画面css追加

--- frontend/app/src/components/model/mypage/ui/ComicConfirmDelete.js
+++ frontend/app/src/components/model/mypage/ui/ComicConfirmDelete.js
@@ -1,27 +1,52 @@
 import { useComic } from "../../../../hooks/useComic";
 import { BsFillTrashFill } from "react-icons/bs";
-import parts from "../../../../css/ui/parts.module.css";
+import { FcReading, FcFile, FcUpLeft, FcPicture } from "react-icons/fc";
+import { useNavigate, useParams } from "react-router-dom";
+import ReactLoading from "react-loading";
+import confirm from "../../../../css/ui/Confirm.module.css";
 
-const ComicConfirmDelete = ({ comicId, comicTitle }) => {
-  const { useDeleteComic } = useComic();
-  const deleteComic = useDeleteComic(comicId);
+const ComicConfirmDelete = () => {
+  const navigate = useNavigate();
+  const { comic_id, comic_title } = useParams();
 
-  //削除用関数
+  const { useShowComic, useDeleteComic } = useComic();
+  const { data: comic, isLoading } = useShowComic(comic_id);
+  const deleteComic = useDeleteComic(comic_id);
+
+  if(isLoading) return <ReactLoading type="spin" color="blue" className='loading' />
+
   const handleDeleteComic = () => {
-    if (
-      window.confirm(`${comicTitle}を削除しますか？`)
-    ) {
-      deleteComic.mutate();
-      alert(`${comicTitle}を削除しました!`);
-      navigate('/mypage');
-    }
+    deleteComic.mutate();
+    alert(`${comic_title}を削除しました!`);
+    navigate('/mypage');
   };
 
   return (
-    <button className={parts['delete-button']} onClick={handleDeleteComic}>
-      <span className={parts['delete-button-icon']}><BsFillTrashFill /></span>
-      削除
-    </button>
+    <div className={confirm.section}>
+      <div className={confirm.content}>
+        <div className={confirm.article}>
+          <div className={confirm['confirm-title']}>以下の内容を削除してもよろしいですか？</div>
+          <div className={confirm["detail-area"]}>
+            <p className={confirm.detail}><span className={confirm["react-icon"]}><FcReading /></span>漫画のタイトル</p>
+            <div>{ comic.title }</div>
+          </div>
+          <div className={confirm["detail-area"]}>
+            <p className={confirm.detail}><span className={confirm["react-icon"]}><FcFile /></span>漫画のジャンル</p>
+            <div>{ comic.genre }</div>
+          </div>
+          <div className={confirm["outer-image"]}>
+          <p className={confirm.detail}><span className={confirm["react-icon"]}><FcPicture /></span>漫画の画像</p>
+            <img className={confirm.image} src={ comic.image.url } alt='画像' onError={(e) => e.target.src = noimage} />
+          </div>
+          <div className={confirm["detail-area-delete"]}>
+            <button className={confirm['delete-button']} onClick={handleDeleteComic}><span className={confirm['delete-button-icon']}><BsFillTrashFill /></span>削除</button>
+          </div>
+          <div className={confirm["detail-area"]}>
+            <button onClick={() => navigate(-1)} className={confirm.back}><span className={confirm["react-icon"]}><FcUpLeft /></span>{ comic.title }の編集画面へ戻る</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
