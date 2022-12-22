bf21a209b3e2c803f4d96f2005731afadb0207ea
yuki178504 <aerosmit1989x@gmail.com>
2022-12-22 20:41:31 +0900

    [add]users_controllerのストロングパラメータにe_mailを追加

--- backend/app/controllers/api/v1/user/users_controller.rb
+++ backend/app/controllers/api/v1/user/users_controller.rb
@@ -15,6 +15,6 @@ class Api::V1::User::UsersController < SecuredController
   private
 
   def user_params
-    params.permit(:name, :introduction, :image, :url)
+    params.permit(:name, :introduction, :image, :url, :e_mail)
   end
 end
