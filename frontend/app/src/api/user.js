be519d3ed929e2e56c2a21bc790b5ed234102448
yuki178504 <aerosmit1989x@gmail.com>
2022-12-25 05:10:11 +0900

    [add]ユーザー情報更新用のapiオブジェクトとカスタムフック作成

--- frontend/app/src/api/user.js
+++ frontend/app/src/api/user.js
@@ -15,4 +15,17 @@ export const user = {
     });
     return res.data;
   },
+
+  putUser: async (params, UserId, token) => {
+    await axios
+    .put(`${process.env.REACT_APP_DEV_API_URL}/user/users/${UserId}`, params, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    .catch((error) => {
+      console.error(error.response.data);
+    });
+  }
 }
