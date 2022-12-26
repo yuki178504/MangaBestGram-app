6bee779aaa1945e8493ea2f4f94c15cb2da8da03
yuki178504 <aerosmit1989x@gmail.com>
2022-12-26 17:57:30 +0900

    [add]バックエンドのcomicモデルに空での登録を拒否するバリデーションを追加

--- backend/app/models/comic.rb
+++ backend/app/models/comic.rb
@@ -1,5 +1,9 @@
 class Comic < ApplicationRecord
   belongs_to :user
   has_many :scene_posts, dependent: :destroy
+
   mount_uploader :image, FileUploader
+
+  validates :title, presence: true
+  validates :genre, presence: true
 end
