8c8578d1ecb272c5bea6b8c48c3ec3ee56bee83a
yuki178504 <aerosmit1989x@gmail.com>
2022-12-28 04:21:37 +0900

    [add]comicモデルのバリデーションのテストを実装

--- backend/spec/factories/comics.rb
+++ backend/spec/factories/comics.rb
@@ -1,5 +1,8 @@
+# encoding: utf-8
 FactoryBot.define do
   factory :comic do
-    
+    title {"ワンピース"}
+    genre {"アドベンチャー"}
+    user_id {1}
   end
 end
--- backend/spec/models/comic_spec.rb
+++ backend/spec/models/comic_spec.rb
@@ -1,7 +1,20 @@
+# encoding: utf-8
 require 'rails_helper'
 
 RSpec.describe Comic, type: :model do
-  it "タイトルがなければ登録できない"
+  describe "漫画新規作成" do
+    context "異常系" do
+      it "タイトルがなければ登録できない" do
+        comic = Comic.new(title: "")
+        comic.valid?
+        expect(comic.errors[:title]).to include("can't be blank")
+      end
 
-  it "ジャンルがなければ登録できない"
+      it "ジャンルがなければ登録できない" do
+        comic = Comic.new(genre: "")
+        comic.valid?
+        expect(comic.errors[:genre]).to include("can't be blank")
+      end
+    end
+  end
 end
