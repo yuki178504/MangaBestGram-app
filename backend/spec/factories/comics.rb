4c752e47810142fcf7112a2c984266357550c3aa
yuki178504 <aerosmit1989x@gmail.com>
2022-12-27 16:25:26 +0900

    [add]comicモデルをテストするmodelのスペックファイルを作成

---
+++ backend/spec/factories/comics.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :comic do
+    
+  end
+end
---
+++ backend/spec/models/comic_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Comic, type: :model do
+  it "タイトルがなければ登録できない"
+
+  it "ジャンルがなければ登録できない"
+end
