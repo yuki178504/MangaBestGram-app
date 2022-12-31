0729f022ed4289ff09f30995e0d09a8f0255847e
yuki178504 <aerosmit1989x@gmail.com>
2022-12-31 15:22:06 +0900

    [add]request_specにcomics_specを作成

---
+++ backend/spec/requests/api/v1/comics_spec.rb
@@ -0,0 +1,42 @@
+# encoding: utf-8
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Comics", type: :request do
+  describe "非ログインユーザーの漫画閲覧機能" do
+    let!(:comic) { create(:comic) }
+    let!(:related_comics) { create_list(:comic, 4) }
+
+    context "/api/v1/comicsにアクセス時に" do
+      it "正常にレスポンスを返すこと" do
+        get "/api/v1/comics"
+        expect(response).to have_http_status(200)
+      end
+
+      it "bodyに漫画のタイトルが含まれていること" do
+        get "/api/v1/comics"
+        expect(response.body).to include comic.title
+      end
+
+      it "bodyに漫画のジャンルが含まれていること" do
+        get "/api/v1/comics"
+        expect(response.body).to include comic.genre
+      end
+    end
+    context "/api/v1/comics/latestにアクセス時に" do
+      it "正常にレスポンスを返すこと" do
+        get "/api/v1/comics/latest"
+        expect(response).to have_http_status(200)
+      end
+
+      it "bodyに漫画のタイトルが含まれていること" do
+        get "/api/v1/comics/latest"
+        expect(response.body).to include comic.title
+      end
+
+      it "bodyに漫画のジャンルが含まれていること" do
+        get "/api/v1/comics/latest"
+        expect(response.body).to include comic.genre
+      end
+    end
+  end
+end
