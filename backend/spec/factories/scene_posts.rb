901c7e0ca84c3ec3286185b39945e04e4baf9d67
yuki178504 <aerosmit1989x@gmail.com>
2023-01-01 01:39:41 +0900

    [add]request_specにscene_post＿specを作成 FactoryBotのscene_postにカラムを追加

--- backend/spec/factories/scene_posts.rb
+++ backend/spec/factories/scene_posts.rb
@@ -5,5 +5,8 @@ FactoryBot.define do
     association :user
     scene_title {"シーンタイトル"}
     sub_title {"サブタイトル"}
+    scene_date {"2022-12-30"}
+    scene_content {"シーンの内容"}
+    scene_number {1}
   end
 end
