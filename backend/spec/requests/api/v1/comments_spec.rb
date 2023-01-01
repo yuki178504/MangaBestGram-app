3b6775c25b50473e1af9da0016e53eb0e8c5e582
yuki178504 <aerosmit1989x@gmail.com>
2023-01-01 22:34:42 +0900

    [add]comments_specに非ログインユーザーのコメント閲覧機能とユーザーのお気に入り数カウント機能を追加

--- backend/spec/requests/api/v1/comments_spec.rb
+++ backend/spec/requests/api/v1/comments_spec.rb
@@ -3,13 +3,41 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Comments", type: :request do
   describe "非ログインユーザーのコメント閲覧機能" do
-    let!(:comment) { create(:comment) }
-    let(:scene_post) { create(:scene_post) }
+    let!(:comment) { create(:comment, scene_post_id: scene_post.id) }
+    let(:comic) { create(:comic) }
+    let(:scene_post) { create(:scene_post, comic_id: comic.id) }
 
-    context "/api/v1/scene_posts/:scene_post_id/commentsにアクセス時に" do
+    before do
+      get api_v1_scene_post_comments_path(scene_post.id)
+    end
+
+    context "api_v1_scene_post_comments_pathにアクセス時に" do
       it "正常にレスポンスを返すこと" do
-        get api_v1_scene_post_comments_path(scene_post.id)
         expect(response).to have_http_status(200)
+      end
+
+      it "bodyにコメントが含まれていること" do
+        expect(response.body).to include comment.body
+      end
+    end
+  end
+
+  describe "ユーザーのお気に入り数カウント機能" do
+    let(:comic) { create(:comic) }
+    let(:scene_post) { create(:scene_post, comic_id: comic.id) }
+    let!(:favorite) { create(:favorite, scene_post_id: scene_post.id) }
+
+    before do
+      get favorites_count_api_v1_scene_post_comments_path(scene_post.id)
+    end
+
+    context "favorites_count_api_v1_scene_post_comments_pathにアクセス時に" do
+      it "正常にレスポンスを返すこと" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "bodyにscene_post_idが含まれていること" do
+        expect(response.body).to include favorite.scene_post_id.to_s
       end
     end
   end
