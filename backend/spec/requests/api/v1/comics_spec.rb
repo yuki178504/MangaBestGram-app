f901f66c9f8eb35f5e934659dd277d715b7cb2f8
yuki178504 <aerosmit1989x@gmail.com>
2023-01-02 14:01:17 +0900

    [update]リクエストスペックのcomics_specの構文をdescribeをテスト別に分ける記述に変更

--- backend/spec/requests/api/v1/comics_spec.rb
+++ backend/spec/requests/api/v1/comics_spec.rb
@@ -6,46 +6,52 @@ RSpec.describe "Api::V1::Comics", type: :request do
     let!(:comic) { create(:comic) }
     let!(:related_comics) { create_list(:comic, 4) }
 
-    context "/api/v1/comicsにアクセス時に" do
+    before do
+      get api_v1_comics_path
+    end
+
+    context "api_v1_comics_pathにアクセス時に" do
       it "正常にレスポンスを返すこと" do
-        get "/api/v1/comics"
         expect(response).to have_http_status(200)
       end
 
       it "全ての漫画のデータを取得できること" do
-        get "/api/v1/comics"
         json = JSON.parse(response.body)
         expect(json['data'].length).to eq(5)
       end
 
       it "bodyに漫画のタイトルが含まれていること" do
-        get "/api/v1/comics"
         expect(response.body).to include comic.title
       end
 
       it "bodyに漫画のジャンルが含まれていること" do
-        get "/api/v1/comics"
         expect(response.body).to include comic.genre
       end
     end
-    context "/api/v1/comics/latestにアクセス時に" do
+  end
+
+  describe "非ログインユーザーの漫画閲覧一覧を2つに限定した機能" do
+    let!(:comic) { create(:comic) }
+    let!(:related_comics) { create_list(:comic, 4) }
+
+    before do
+      get latest_api_v1_comics_path
+    end
+
+    context "latest_api_v1_comics_pathにアクセス時に" do
       it "正常にレスポンスを返すこと" do
-        get "/api/v1/comics/latest"
         expect(response).to have_http_status(200)
       end
 
       it "bodyに漫画のタイトルが含まれていること" do
-        get "/api/v1/comics/latest"
         expect(response.body).to include comic.title
       end
 
       it "bodyに漫画のジャンルが含まれていること" do
-        get "/api/v1/comics/latest"
         expect(response.body).to include comic.genre
       end
 
       it "表示される漫画のデータが2つであること" do
-        get "/api/v1/comics/latest"
         json = JSON.parse(response.body)
         expect(json['data'].length).to eq(2)
       end
