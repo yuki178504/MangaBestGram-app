a778a6d9e041dd983440b2ab9c0097adbe1e7dc5
yuki178504 <aerosmit1989x@gmail.com>
2023-01-04 13:15:49 +0900

    [add]リクエストスペックのuserディレクトリに認証が必要なコントローラのテストのためのscene_posts_specを作成

--- backend/app/controllers/api/v1/user/scene_posts_controller.rb
+++ backend/app/controllers/api/v1/user/scene_posts_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::User::ScenePostsController < SecuredController
   before_action :set_scene_post, only: %i(show update destroy)
 
   def index
-    posts = @current_user.comics.find_by!(id: params[:comic_id]).scene_posts.all.order(id: :desc)
+    posts = current_user.comics.find_by!(id: params[:comic_id]).scene_posts.all.order(id: :desc)
     render json: posts
   end
 
@@ -11,8 +11,8 @@ class Api::V1::User::ScenePostsController < SecuredController
   end
 
   def create
-    post = @current_user.comics.find_by!(id: params[:comic_id]).scene_posts.build(scene_post_params)
-    if post.save!
+    post = current_user.comics.find_by!(id: params[:comic_id]).scene_posts.build(scene_post_params)
+    if post.save
       render json: post
     else
       render json: post.errors, status: :unprocessable_entity
@@ -30,12 +30,12 @@ class Api::V1::User::ScenePostsController < SecuredController
   private
 
   def set_scene_post
-    @scene_post = @current_user.scene_posts.find_by!(id: params[:id])
+    @scene_post = current_user.scene_posts.find_by!(id: params[:id])
   end
 
   def scene_post_params
     params.permit(:scene_title, :scene_date, :scene_content, :scene_image, :scene_number, :sub_title).
-      merge(user_id: @current_user.id)
+      merge(user_id: current_user.id)
   end
 
   def scene_post_update_params
