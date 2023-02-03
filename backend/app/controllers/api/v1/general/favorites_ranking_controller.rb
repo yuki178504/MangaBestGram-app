4d0bf4fd7bf79be15d45725fbac94da54c19637e
yuki178504 <aerosmit1989x@gmail.com>
2023-02-03 23:47:43 +0900

    [fix]シーンのランキング表示を10個に限定した状態から並び替える状態になっていたので、全投稿の中からいいねされた投稿が一つ以上ある状態の投稿を10個に限定して並び替える記述に変更

--- backend/app/controllers/api/v1/general/favorites_ranking_controller.rb
+++ backend/app/controllers/api/v1/general/favorites_ranking_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::General::FavoritesRankingController < SecuredController
   def favorites_ranking
-    scene_posts = ScenePost.includes(:favorite_users).limit(10).sort { |a, b| b.favorite_users.size <=> a.favorite_users.size }
+    scene_posts = ScenePost.find(Favorite.group(:scene_post_id).order('count(scene_post_id) desc').limit(10).pluck(:scene_post_id))
     render_json = General::ScenePostSerializer.new(scene_posts, current_user: @current_user).serializable_hash.to_json
     render json: render_json
   end
