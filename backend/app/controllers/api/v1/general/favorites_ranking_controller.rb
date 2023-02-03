05b0cec498f8dad333e74df8f73cfe63ecd7788a
yuki178504 <aerosmit1989x@gmail.com>
2023-02-04 00:02:55 +0900

    [fix]rubocopのエラーを修正

--- backend/app/controllers/api/v1/general/favorites_ranking_controller.rb
+++ backend/app/controllers/api/v1/general/favorites_ranking_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::General::FavoritesRankingController < SecuredController
   def favorites_ranking
-    scene_posts = ScenePost.find(Favorite.group(:scene_post_id).order('count(scene_post_id) desc').limit(10).pluck(:scene_post_id))
+    scene_posts = ScenePost.find(Favorite.group(:scene_post_id).order('count(scene_post_id) desc').limit(10).
+                  pluck(:scene_post_id))
     render_json = General::ScenePostSerializer.new(scene_posts, current_user: @current_user).serializable_hash.to_json
     render json: render_json
   end
