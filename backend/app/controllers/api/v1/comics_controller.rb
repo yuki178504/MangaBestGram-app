299b21f54880cd4b25f710799a85d36ea2bfeb40
yuki178504 <aerosmit1989x@gmail.com>
2023-05-05 01:17:22 +0900

    [update]Api::V1::ComicsControllerのto_jsonメソッドを不要なので削除

--- backend/app/controllers/api/v1/comics_controller.rb
+++ backend/app/controllers/api/v1/comics_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::ComicsController < ApplicationController
   def index
     comics = Comic.all.order(id: :desc)
-    render_json = ComicSerializer.new(comics).serializable_hash.to_json
+    render_json = ComicSerializer.new(comics).serializable_hash
     render json: render_json
   end
 
   def latest
     comics = Comic.order(updated_at: :desc).limit(10)
-    render_json = ComicSerializer.new(comics).serializable_hash.to_json
+    render_json = ComicSerializer.new(comics).serializable_hash
     render json: render_json
   end
 end
